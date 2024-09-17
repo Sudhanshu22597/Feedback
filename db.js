32cb6139fe087ea240ab96c07f5f50cfcae2a5b9
Sudhanshu22597 <sudhanshukumar22597@gmail.com>
2024-09-17 21:35:49 +0530

    a

---
+++ db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+module.exports = { client, connectToDatabase };
